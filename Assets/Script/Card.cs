a303f9d766182e9e62be4f8e0661de9f9102453a
seb <sbroussaud@boostr.net>
2016-11-14 22:50:42 +0100

    add deck + card definition

--- Assets/Script/Card.cs
+++ Assets/Script/Card.cs
@@ -1,22 +1,148 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Linq;
+using System;
 
-public class Card : MonoBehaviour {
-
+public class Card : MonoBehaviour
+{
     public Text AttackText;
     public Text DefenceText;
     public Text StarText;
+    public Text RarityText;
+    public Image Sign;
+    public Text[] Powers;
+    public Animator Animator;
+    public GameEngine Engine;
 
+    public GameplayCard GpCard;
 
+    public bool Show;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        this.Animator.SetBool("Show", Show);
 	}
+
+    public void OnClick()
+    {
+        if (Engine != null)
+        {
+            Engine.OnClick(this);
+        }
+    }
+
+    public void Assign(GameplayCard gameplayCard)
+    {
+        AttackText.text = gameplayCard.Attack.ToString();
+        DefenceText.text = gameplayCard.Defence.ToString();
+        StarText.text = gameplayCard.Level.ToString();
+        RarityText.text = gameplayCard.Rarity.ToString()[0].ToString();
+        Sign.sprite = gameplayCard.Definition[0].CardImage;
+        Powers[0].text = gameplayCard.Definition[0].CardPower.ToString();
+        Powers[1].text = "";
+    }
+}
+
+[System.Serializable]
+public class GameplayCard
+{
+    public GameplayCard(CardDefinition.Rarity rarity, int level, params CardDefinition[] definitions)
+    {
+        Definition = definitions;
+        Rarity = rarity;
+        Level = level;
+    }
+
+    public CardDefinition[] Definition;
+    public CardDefinition.Rarity Rarity;
+    public int Level;
+
+    public int Attack
+    {
+        get
+        {
+            return Definition.Sum(x => x.GetAttack(Level)) / Definition.Length;
+        }
+    }
+
+    public int Defence
+    {
+        get
+        {
+            return Definition.Sum(x => x.GetDefense(Level)) / Definition.Length;
+        }
+    }
+}
+
+[System.Serializable]
+public class CardDefinition
+{
+    public enum Sign
+    {
+        Aries,
+        Taurus,
+        Gemini,
+        Cancer,
+        Leo,
+        Virgo,
+        Libra,
+        Scorpio,
+        Sagittarius,
+        Capricorn,
+        Aquarius,
+        Pisces,
+    }
+
+    public enum Rarity
+    {
+        Common,
+        Uncommon,
+        Rare,
+        Legendary,
+        Mythic
+    }
+
+    public enum Power
+    {
+        Shield,
+        Rage,
+        Multiple,
+        Curse,
+        Zone,
+        Retribute,
+        Armor,
+        Poison,
+        Speed,
+        Trample,
+        Ice,
+        Regeneration,
+    }
+
+    public Sign CardSign;
+    public Sprite CardImage;
+    public Power CardPower;
+    public int BaseAttack;
+    public int BaseDefense;
+    public float AttackIncrement;
+    public float DefenceIncrement;
+
+    public int GetAttack(int level)
+    {
+        int increment = (int)(AttackIncrement * ((float)level - 1f));
+        return BaseAttack + increment;
+    }
+
+    public int GetDefense(int level)
+    {
+        int increment = (int)(DefenceIncrement * ((float)level - 1f));
+        return BaseDefense + increment;
+    }
 }
---
+++ Assets/Script/GameEngine.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+public class GameEngine : MonoBehaviour
+{
+    public CardDefinition[] Definition;
+    public Card[] PlayableCards;
+    public Card[] TopCards;
+    public Card[] MiddleCards;
+
+    private List<GameplayCard> MyDeck = new List<GameplayCard>();
+    private List<GameplayCard> OtherDeck = new List<GameplayCard>();
+
+    private System.Random Random = new System.Random();
+    private bool IsInit = false;
+     
+	// Use this for initialization
+	void Start ()
+    {
+        InitDeck(MyDeck);
+        InitDeck(OtherDeck);
+
+        StartCoroutine(ShowCard());
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	}
+
+    public IEnumerator ShowCard()
+    {
+        for (int i = 0; i < 5; ++i)
+        {
+            yield return new WaitForSeconds(1);
+            PlayableCards[i].Assign(MyDeck[i]);
+            PlayableCards[i].Show = true;
+        }
+
+        IsInit = true;
+    }
+
+    public void InitDeck(List<GameplayCard> deck)
+    {
+        List<GameplayCard> baseDeck = new List<GameplayCard>();
+        foreach (CardDefinition def in Definition)
+        {
+            baseDeck.Add(new GameplayCard(CardDefinition.Rarity.Common, 1, def));
+        }
+
+        while (baseDeck.Count != 0)
+        {
+            int randomIndex = Random.Next(baseDeck.Count);
+            deck.Add(baseDeck[randomIndex]);
+            baseDeck.RemoveAt(randomIndex);
+        }
+    }
+
+    public void OnClick(Card card)
+    {
+        if (!IsInit)
+        {
+            return;
+        }
+
+        if (PlayableCards.Contains(card))
+        {
+
+        }
+    }
+}
